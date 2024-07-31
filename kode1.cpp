10edb9509bf2f55b9276c5fa48591be2b905ccd6
CintaDesvinaAnggreyanny <23_cintadesvina@student.smkti.net>
2024-07-31 11:48:04 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+void cetakData(string nama, string nisn, string jurusan, int tinggiBadan){
+    cout << "nama anda: "<< nama << endl;
+    cout << "nisn anda: "<< nisn << endl;
+    cout << "jurusan anda: "<< jurusan << endl;
+    cout << "tinggi badan: "<< tinggiBadan << endl;
 }
+
+int main (){
+    string nama = "Cinta Desvina Anggreyanny";
+    string nisn = "0072345491";
+    string jurusan = "PPLG";
+    int tinggiBadan = 148;
+
+     cetakData(nama, nisn, jurusan, tinggiBadan);
+
+return 0;
+}
+  
